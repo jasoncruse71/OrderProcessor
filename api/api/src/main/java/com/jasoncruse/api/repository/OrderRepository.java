b4191bc123f6bde9a8f2b7d9c44679ccc98de476
Jason Cruse <jasoncruse2002@gmail.com>
2026-08-20 16:53:35 -0700

    order repository interface

--- api/api/src/main/java/com/jasoncruse/api/repository/OrderRepository.java
+++ api/api/src/main/java/com/jasoncruse/api/repository/OrderRepository.java
@@ -1,4 +1,10 @@
 package com.jasoncruse.api.repository;
 
-public class OrderRepository {
+import com.jasoncruse.api.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+    List<Order> findByCustomerEmail(String customerEmail);
 }
