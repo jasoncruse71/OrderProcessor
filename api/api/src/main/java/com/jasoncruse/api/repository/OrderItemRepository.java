5b41e7cd4f9b1f1a67b35592fa34cc42ab8dcdc8
Jason Cruse <jasoncruse2002@gmail.com>
2026-08-20 16:54:53 -0700

    order item repository interface

--- api/api/src/main/java/com/jasoncruse/api/repository/OrderItemRepository.java
+++ api/api/src/main/java/com/jasoncruse/api/repository/OrderItemRepository.java
@@ -1,4 +1,8 @@
 package com.jasoncruse.api.repository;
 
-public class OrderItemRepository {
+import com.jasoncruse.api.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
 }
